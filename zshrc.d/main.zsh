17cf08b23a95306b8e3fba00e6f9befab69b428f
Chris Gibson <christopher.gibson@instacart.com>
2022-02-17 14:12:15 -0600

    Fix zsh

--- zshrc.d/main.zsh
+++ zshrc.d/main.zsh
@@ -1,5 +1,3 @@
-export ZSH=$HOME/.oh-my-zsh
-
 ZSH_THEME="gibson"
 
 plugins=(
@@ -9,6 +7,8 @@ plugins=(
   history-substring-search
   shrink-path
 )
+
+source $ZSH/oh-my-zsh.sh
 
 export EDITOR='nvim'
 
